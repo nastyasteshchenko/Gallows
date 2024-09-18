7bfd783667365f7aac66ac499134d1499e45ba5b
anastasiya <a.steshchenko@g.nsu.ru>
2024-09-18 14:45:01 +0700

    feat: add JsonProperty to enum

--- src/main/java/backend/academy/model/Difficulty.java
+++ src/main/java/backend/academy/model/Difficulty.java
@@ -1,7 +1,12 @@
 package backend.academy.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public enum Difficulty {
+    @JsonProperty("easy")
     EASY,
+    @JsonProperty("medium")
     MEDIUM,
+    @JsonProperty("hard")
     HARD
 }
