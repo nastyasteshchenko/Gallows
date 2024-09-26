6c871041324c9a1323302b28d03eeefabf7687a6
anastasiya <a.steshchenko@g.nsu.ru>
2024-09-26 16:50:13 +0700

    fix: fix guessing letter

--- src/test/java/backend/academy/model/GameStateTest.java
+++ src/test/java/backend/academy/model/GameStateTest.java
@@ -1,5 +1,6 @@
 package backend.academy.model;
 
+import backend.academy.model.action.GuessLetterAction;
 import backend.academy.model.word.Word;
 import backend.academy.view.GameInfo;
 import org.junit.jupiter.api.Test;
@@ -7,18 +8,23 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class GameStateTest {
 
+    private final Model model = new Model();
+
     @Test
     void isWinTest() {
         Word word = new Word("test");
         GameState gameState = new GameState(Difficulty.EASY, "test", word);
+        model.currentGameState(gameState);
 
-        gameState.guessLetter("t");
+        GuessLetterAction guessLetterAction = new GuessLetterAction(model);
+
+        guessLetterAction.accept("t");
         assertFalse(gameState.isWin());
 
-        gameState.guessLetter("E");
+        guessLetterAction.accept("E");
         assertFalse(gameState.isWin());
 
-        gameState.guessLetter("s");
+        guessLetterAction.accept("s");
         assertTrue(gameState.isWin());
     }
 
@@ -26,23 +32,26 @@ class GameStateTest {
     void isLooseTest() {
         Word word = new Word("test");
         GameState gameState = new GameState(Difficulty.HARD, "test", word);
+        model.currentGameState(gameState);
 
-        gameState.guessLetter("t");
+        GuessLetterAction guessLetterAction = new GuessLetterAction(model);
+
+        guessLetterAction.accept("t");
         assertFalse(gameState.isLoose());
 
-        gameState.guessLetter("M");
+        guessLetterAction.accept("M");
         assertFalse(gameState.isLoose());
 
-        gameState.guessLetter("s");
+        guessLetterAction.accept("s");
         assertFalse(gameState.isLoose());
 
-        gameState.guessLetter("B");
+        guessLetterAction.accept("B");
         assertFalse(gameState.isLoose());
 
-        gameState.guessLetter("v");
+        guessLetterAction.accept("v");
         assertFalse(gameState.isLoose());
 
-        gameState.guessLetter("a");
+        guessLetterAction.accept("a");
         assertTrue(gameState.isLoose());
     }
 
@@ -50,8 +59,11 @@ class GameStateTest {
     void isAlreadyUsedTest() {
         Word word = new Word("test");
         GameState gameState = new GameState(Difficulty.EASY, "test", word);
+        model.currentGameState(gameState);
 
-        gameState.guessLetter("t");
+        GuessLetterAction guessLetterAction = new GuessLetterAction(model);
+
+        guessLetterAction.accept("t");
         assertTrue(gameState.isAlreadyUsed("t"));
 
         assertFalse(gameState.isAlreadyUsed("E"));
@@ -72,8 +84,11 @@ class GameStateTest {
     void guessLetterTest() {
         Word word = new Word("test");
         GameState gameState = new GameState(Difficulty.EASY, "test", word);
+        model.currentGameState(gameState);
 
-        gameState.guessLetter("t");
+        GuessLetterAction guessLetterAction = new GuessLetterAction(model);
+
+        guessLetterAction.accept("t");
         assertEquals("t _ _ t", gameState.word().toString());
     }
 
@@ -81,6 +96,9 @@ class GameStateTest {
     void wordTest() {
         Word word = new Word("test");
         GameState gameState = new GameState(Difficulty.EASY, "test", word);
+        model.currentGameState(gameState);
+
+        GuessLetterAction guessLetterAction = new GuessLetterAction(model);
 
         assertEquals(word, gameState.word());
     }
@@ -96,7 +114,10 @@ class GameStateTest {
         assertEquals("test", gameInfo.theme());
         assertEquals("A B C D E F G H I J K L M N O P Q R S T U V W X Y Z", gameInfo.alphabet());
 
-        gameState.guessLetter("t");
+        model.currentGameState(gameState);
+        GuessLetterAction guessLetterAction = new GuessLetterAction(model);
+
+        guessLetterAction.accept("t");
         gameInfo = gameState.getGameInfo();
         assertEquals("t _ _ t", gameInfo.wordLetters());
         assertEquals(Difficulty.EASY.attempts(), gameInfo.attemptsLeft());
@@ -104,7 +125,7 @@ class GameStateTest {
         assertEquals("test", gameInfo.theme());
         assertEquals("A B C D E F G H I J K L M N O P Q R S U V W X Y Z", gameInfo.alphabet());
 
-        gameState.guessLetter("M");
+        guessLetterAction.accept("M");
         gameInfo = gameState.getGameInfo();
         assertEquals("t _ _ t", gameInfo.wordLetters());
         assertEquals(Difficulty.EASY.attempts() - 1, gameInfo.attemptsLeft());
