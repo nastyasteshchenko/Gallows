c8cbd126efbb5714e8a6e4982a747b8d1fad4348
anastasiya <a.steshchenko@g.nsu.ru>
2024-09-26 13:34:34 +0700

    fix: add checking if dictionary is null

--- src/test/java/backend/academy/model/DictionaryTest.java
+++ src/test/java/backend/academy/model/DictionaryTest.java
@@ -8,23 +8,28 @@ import org.junit.jupiter.api.Test;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class DictionaryTest {
 
     @Test
-    void gettingRandomWordTest() throws UnsupportedFileContentException, IOException {
+    void gettingRandomWordTest() throws IOException {
         Dictionary dictionary = Dictionary.loadDictionary();
 
-        String theme = dictionary.getRandomTheme();
-        Difficulty difficulty = Difficulty.getRandomDifficulty();
-        String word = dictionary.getRandomWord(theme, difficulty);
+        assertNotEquals(null, dictionary);
 
-        ObjectMapper mapper = JsonMapper.builder().build();
-        TmpDictionary tmpDictionary =
-            mapper.readValue(ClassLoader.getSystemResource("dictionary.json"), TmpDictionary.class);
+        if (dictionary != null) {
+            String theme = dictionary.getRandomTheme();
+            Difficulty difficulty = Difficulty.getRandomDifficulty();
+            String word = dictionary.getRandomWord(theme, difficulty);
 
-        assertTrue(tmpDictionary.dictionary().get(theme).get(difficulty).contains(word));
+            ObjectMapper mapper = JsonMapper.builder().build();
+            TmpDictionary tmpDictionary =
+                mapper.readValue(ClassLoader.getSystemResource("dictionary.json"), TmpDictionary.class);
+
+            assertTrue(tmpDictionary.dictionary().get(theme).get(difficulty).contains(word));
+        }
     }
 
     @Getter
