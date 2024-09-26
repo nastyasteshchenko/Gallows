fb2e8b6e2802daadec2f85543892cdcd68df31b0
anastasiya <a.steshchenko@g.nsu.ru>
2024-09-26 12:29:30 +0700

    feat: add new class GameInitializer to initialize Model, View and Controller

---
+++ src/main/java/backend/academy/GameInitializer.java
@@ -0,0 +1,47 @@
+package backend.academy;
+
+import backend.academy.controller.Controller;
+import backend.academy.model.Model;
+import backend.academy.view.GameView;
+import backend.academy.view.View;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class GameInitializer {
+    private static final Logger LOGGER = LogManager.getLogger(GameInitializer.class);
+
+    Controller init() {
+        Model model = new Model();
+        Controller controller = new Controller();
+        View view = new View();
+
+        setModelListener(model, view);
+        setViewListener(view.gameView(), model);
+        setControllerListener(controller, model);
+        return controller;
+    }
+
+    private static void setModelListener(Model model, View view) {
+        model.chooseThemeListener(view.choosingThemeMenu());
+        model.chooseDifficultyListener(view.choosingDifficultyMenu());
+        model.drawGameListener(view.gameView());
+        model.gameWinListener(view.gameView());
+        model.gameLooseListener(view.gameView());
+        model.guessLetterListener(view.gameView());
+        model.alreadyUsedLetterListener(view.gameView());
+        model.notInAlphabetListener(view.gameView());
+        model.errorListener(view.gameView());
+        LOGGER.info("Model listeners were set.");
+    }
+
+    private static void setViewListener(GameView gameView, Model model) {
+        gameView.enterLetterListener(model);
+        gameView.continueGameListener(model);
+        LOGGER.info("View listeners were set.");
+    }
+
+    private static void setControllerListener(Controller controller, Model model) {
+        controller.startNewGameListener(model);
+        LOGGER.info("Controller listeners were set.");
+    }
+}
--- src/main/java/backend/academy/Main.java
+++ src/main/java/backend/academy/Main.java
@@ -1,9 +1,6 @@
 package backend.academy;
 
 import backend.academy.controller.Controller;
-import backend.academy.model.Model;
-import backend.academy.view.GameView;
-import backend.academy.view.View;
 import lombok.experimental.UtilityClass;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -15,39 +12,9 @@ public class Main {
 
     public static void main(String[] args) {
         LOGGER.info("Program was started.");
-        Model model = new Model();
-        Controller controller = new Controller();
-        View view = new View();
-
-        setModelListener(model, view);
-        setViewListener(view.gameView(), model);
-        setControllerListener(controller, model);
-
+        GameInitializer gameInitializer = new GameInitializer();
+        Controller controller = gameInitializer.init();
         controller.startNewGame();
         LOGGER.info("Program was finished.");
-    }
-
-    private static void setModelListener(Model model, View view) {
-        model.chooseThemeListener(view.choosingThemeMenu());
-        model.chooseDifficultyListener(view.choosingDifficultyMenu());
-        model.drawGameListener(view.gameView());
-        model.gameWinListener(view.gameView());
-        model.gameLooseListener(view.gameView());
-        model.guessLetterListener(view.gameView());
-        model.alreadyUsedLetterListener(view.gameView());
-        model.notInAlphabetListener(view.gameView());
-        model.errorListener(view.gameView());
-        LOGGER.info("Model listeners were set.");
-    }
-
-    private static void setViewListener(GameView gameView, Model model) {
-        gameView.enterLetterListener(model);
-        gameView.continueGameListener(model);
-        LOGGER.info("View listeners were set.");
-    }
-
-    private static void setControllerListener(Controller controller, Model model) {
-        controller.startNewGameListener(model);
-        LOGGER.info("Controller listeners were set.");
     }
 }
